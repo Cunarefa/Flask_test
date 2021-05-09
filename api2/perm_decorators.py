10e9ec2541a1b254a7579a8af32d6ca0330de601
Yevgen <yevgen.yelik@gmail.com>
2021-05-09 17:38:38 +0300

    add perm_decorators

--- api2/perm_decorators.py
+++ api2/perm_decorators.py
@@ -5,18 +5,12 @@ from flask_jwt_extended import current_user
 from api2.models.enums import Role
 
 
-class roles_required:
-
-    def __init__(self, role):
-        self.role = role
-
-    def __call__(self, fun):
+def roles_required(role):
+    def decorator(fun):
         @wraps(fun)
-        def decorator(*args, **kwargs):
-            if not current_user or (current_user.role != Role.ADMIN and current_user.role.value != self.role):
+        def decorated(*args, **kwargs):
+            if not current_user or (current_user.role != Role.ADMIN and current_user.role != role):
                 abort(403)
             return fun(*args, **kwargs)
-
-        return decorator
-
-
+        return decorated
+    return decorator
